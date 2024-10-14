ae8094c61606934b29a0a9d33d2e20a49e4795e9
yx <yx@testemail.com>
2024-10-14 17:00:40 +0800

    not working pt1

--- input_2.s
+++ input_2.s
@@ -22,54 +22,49 @@ output_msg:
 
 _start:
 
-        // Read input from the user
-        mov rax, sys_read
-        mov rdi, stdin
-        lea eax, [b1]
-        mov rdx, buffer_len
-        syscall
+    // Read input from the user
+    mov rax, sys_read
+    mov rdi, stdin
+    lea eax, [b1]
+    mov rdx, buffer_len
+    syscall
 
-        // sys_write
-        mov rax, sys_write
-        mov rdi, stdout
-        lea rsi, [output_msg]
-        mov rdx, 13
-        syscall
+    // output_msg
+    mov rax, sys_write
+    mov rdi, stdout
+    lea rsi, [output_msg]
+    mov rdx, 13
+    syscall
 
-        // Write whatever the user entered back out
-        mov rax, sys_write
-        mov rdi, stdout
-        lea rsi, eax
-        mov rdx, rax
-        syscall
+    //2^input_1 * 2^input_2 * input_3 / 8(bits --> bytes)
+    //exponent loop
+    mov ebx, eax
+    call power
+    mov eax, edx
 
-        //2^input_1 * 2^input_2 * input_3 / 8(bits --> bytes)
-        //exponent loop
-        mov ebx, eax //b1 == 11 input is exponent
-        call pw
-        mov eax, edx
+    // Output of func
+    mov rax, sys_write
+    mov rdi, stdout
+    lea rsi, [eax]
+    mov rdx, 14
+    syscall
 
-        // Output of func
-        mov rax, sys_write
-        mov rdi, stdout
-        lea rsi, eax
-        mov rdx, 14
-        syscall
+    // End the program
+    mov rax, sys_exit
+    mov rdi, success
+    syscall
 
-        // End the program
-        mov rax, sys_exit
-        mov rdi, success
-        syscall
+power:
+    //jl(jump if less than)
+    //jg(jump if more than)
+    mov ecx, 0
 
-pw:
-        //jl(jump if less than)
-        //jg(jump if more than)
-        mov ecx, 0 //i
-        mov edx, 1 //result
-        cmp ecx, ebx
-        jl ret // exit loop if i < result
+    cmp ecx, ebx
 
-        mul edx, edx, 2 //multiply result by 2 until loop ends exponent is 2*2*2*2*...*2
+    mov edx, 1
+    mov ax, 2
+    mul edx
+    //edx = edx * ax multiply result by 2 until loop ends exponent is 2*2*2*2*...*2
 
-
+    ret
 
